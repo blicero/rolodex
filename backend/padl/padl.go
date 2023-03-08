47e2fc91d92a04f2e70358a8ee46717f6ac8a4bd
Benjamin Walkenhorst <krylon@posteo.de>
2023-03-08 21:56:56 +0100

    Rename package backend/ldap to backend/padl to avoid naming clashes

--- backend/padl/padl.go
+++ backend/padl/padl.go
@@ -2,10 +2,12 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 07. 03. 2023 by Benjamin Walkenhorst
 // (c) 2023 Benjamin Walkenhorst
-// Time-stamp: <2023-03-08 18:55:58 krylon>
+// Time-stamp: <2023-03-08 21:56:36 krylon>
 
-// Package ldap uses an LDAP directory as its backend.
-package ldap
+// Package padl uses an LDAP directory as its backend.
+// I wanted to name the package ldap, which would have been the obvious name,
+// but that clashes, of course, with go-ldap. So padl is ldap spelled backwards.
+package padl
 
 import (
 	"log"
