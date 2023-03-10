6f05a4d975fc5bfda518d466033e2d1a685b9389
Benjamin Walkenhorst <krylon@posteo.de>
2023-03-10 16:58:37 +0100

    Update LDAP search script to use a user-supplied server

--- backend/padl/testdata/search.sh
+++ backend/padl/testdata/search.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-# Time-stamp: <2023-03-10 16:04:07 krylon>
+# Time-stamp: <2023-03-10 16:33:15 krylon>
 
-ldapsearch -x -b "ou=contacts,dc=krylon,dc=net" -s sub -a always -l 10 -H "ldap://achtfaden" -P 3 "(sn=Simpson)" sn givenName
+ldapsearch -x -b "ou=contacts,dc=krylon,dc=net" -s sub -a always -l 10 -H "ldap://$1/" -P 3 "(sn=Simpson)" sn givenName
+
 
